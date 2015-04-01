fba0e7c36c6f3ad7e872ccb2d9e93f2447c9c188
Xyxen <XyxenGM@gmail.com>
2015-04-01 15:01:33 -0600

    Initial version of schema

---
+++ init.sql
@@ -0,0 +1,101 @@
+
+CREATE TABLE employee (
+    fname           VARCHAR(32)     NOT NULL,
+    lname           VARCHAR(32)     NOT NULL,
+    sin             CHAR(9),
+    birthdate       DATE            NOT NULL,
+    s_sin           CHAR(9),
+
+    PRIMARY KEY sin,
+    FOREIGN KEY s_sin REFERENCES employee(sin)
+);
+
+CREATE TABLE emp_mname (
+    mname           VARCHAR(32)     NOT NULL,
+    e_sin           CHAR(9),
+
+    FOREIGN KEY s_sin REFERENCES employee(sin)
+);
+
+CREATE TABLE dependent (
+    name            VARCHAR(64)     NOT NULL,
+    birthdate       DATE,
+    relship         VARCHAR(16)     NOT NULL,
+    e_sin           CHAR(9),
+
+    FOREIGN KEY e_sin REFERENCES employee(sin)
+);
+
+CREATE TABLE section (
+    name            VARCHAR(32)     NOT NULL,
+    num             INT,
+    s_sin           CHAR(9),
+
+    PRIMARY KEY num,
+    FOREIGN KEY s_sin REFERENCES employee(sin)
+);
+
+CREATE TABLE cell (
+    num             INT             NOT NULL,
+    s_num           INT,
+
+    FOREIGN KEY s_num REFERENCES section(num)
+);
+
+CREATE TABLE shift (
+    req_role        VARCHAR(10),
+    start           DATE            NOT NULL,
+    end             DATE            NOT NULL,
+    e_sin           CHAR(9),
+    s_num           INT,
+
+    PRIMARY KEY e_sin,
+    FOREIGN KEY e_sin REFERENCES employee(sin),
+    FOREIGN KEY s_num REFERENCES section(num)
+);
+
+CREATE TABLE task (
+    id              INT,
+    desc            VARCHAR(512),
+    start           DATE            NOT NULL,
+    end             DATE            NOT NULL,
+    s_sin           CHAR(9),
+    s_num           INT,
+
+    PRIMARY KEY id,
+    FOREIGN KEY s_sin REFERENCES employee(sin),
+    FOREIGN KEY s_num REFERENCES section(num)
+);
+
+CREATE TABLE equipment (
+    name            VARCHAR(32)     NOT NULL,
+    t_id            INT,
+
+    FOREIGN KEY t_id REFERENCES task(id)
+);
+
+CREATE TABLE detainee (
+    fname           VARCHAR(32)     NOT NULL,
+    lname           VARCHAR(32)     NOT NULL,
+    sin             CHAR(9),
+    birthdate       DATE            NOT NULL,
+    rel_date        DATE            NOT NULL,
+    -- CELL?
+);
+
+CREATE TABLE works (
+    d_sin           CHAR(9),
+    t_id            INT,
+
+    FOREIGN KEY d_sin REFERENCES detainee(sin),
+    FOREIGN KEY t_id REFERENCES task(id)
+);
+
+CREATE TABLE contact (
+    name            VARCHAR(64)     NOT NULL,
+    birthdate       DATE,
+    relship         VARCHAR(16)     NOT NULL,
+    d_sin           CHAR(9),
+
+    FOREIGN KEY d_sin REFERENCES detainee(sin)
+);
