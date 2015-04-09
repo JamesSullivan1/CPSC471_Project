4b0fe9d020b5bf6fc5833f54709418dafaa9ffcd
JamesSullivan1 <calgaryjames@gmail.com>
2015-04-09 15:27:57 -0600

    Updated init sql script with cell table

--- data.sql
+++ data.sql
@@ -3,6 +3,7 @@ INSERT INTO employee VALUES ('123456789',NULL,'mary');
 INSERT INTO people VALUES ('joe',SHA1('joe'),'joe','smith',NULL);
 INSERT INTO employee VALUES ('111111111',NULL,'joe');
 INSERT INTO people VALUES ('bob',SHA1('bob'),'bob','bob',NULL);
+INSERT INTO detainee VALUES ('bob','1900-01-01');
 
 INSERT INTO section VALUES ('cafeteria',1,'123456789');
 
---
+++ detainee_management.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'connect.php';
+require_once 'member.php';
+
+function addDetainee($uname, $pass, $fname, $lname, $birthdate, $rel_date)
+{
+
+}
+
+?>
