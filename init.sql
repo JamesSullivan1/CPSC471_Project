6ddb06d79182298d5118c39a4d0252739de62d95
JamesSullivan1 <calgaryjames@gmail.com>
2015-04-09 15:48:04 -0600

    removed sin from detainee

--- init.sql
+++ init.sql
@@ -96,32 +96,31 @@ CREATE TABLE IF NOT EXISTS equipment (
 );
 
 CREATE TABLE IF NOT EXISTS detainee (
-    sin             CHAR(9),
-    rel_date        DATETIME            NOT NULL,
     uname           VARCHAR(64)     UNIQUE NOT NULL,
+    rel_date        DATETIME            NOT NULL,
     -- CELL?
 
-    PRIMARY KEY (sin),
+    PRIMARY KEY (uname),
     FOREIGN KEY (uname) REFERENCES people(uname) 
     ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS works (
-    d_sin           CHAR(9),
+    d_uname         VARCHAR(64),
     t_id            INT,
 
-    FOREIGN KEY (d_sin) REFERENCES detainee(sin)
+    FOREIGN KEY (d_uname) REFERENCES detainee(uname)
     ON DELETE CASCADE,
     FOREIGN KEY (t_id) REFERENCES task(id)
     ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS livesin (
-    d_sin           CHAR(9),
+    d_uname         VARCHAR(64),
     s_num           INT,
     c_num           INT,
 
-    FOREIGN KEY (d_sin) REFERENCES detainee(sin)
+    FOREIGN KEY (d_uname) REFERENCES detainee(uname)
     ON DELETE CASCADE,
     FOREIGN KEY (s_num) REFERENCES section(num)
     ON DELETE CASCADE,
@@ -133,9 +132,9 @@ CREATE TABLE IF NOT EXISTS contact (
     name            VARCHAR(64)     NOT NULL,
     birthdate       DATETIME,
     relship         VARCHAR(16)     NOT NULL,
-    d_sin           CHAR(9),
+    d_uname         VARCHAR(64),
 
-    FOREIGN KEY (d_sin) REFERENCES detainee(sin)
+    FOREIGN KEY (d_uname) REFERENCES detainee(uname)
     ON DELETE CASCADE
 );
 
